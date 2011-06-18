5ff2cdd9032d2e2196d5d18a390a8ede26fe5f59
Amir Moulavi <amir.moulavi@gmail.com>
2011-06-18 22:04:50 +0200

    Problem 12 solved

---
+++ src/main/scala/p12.scala
@@ -0,0 +1,18 @@
+/**
+* @author Amir Moulavi
+* Problem 12: Decode a run-length encoded list.
+*
+*/
+
+object p12 {
+
+  def decode[A](list:List[(Int,A)]):List[A] = list match {
+    case head :: tail => expand(head) ::: decode(tail)
+    case Nil => Nil
+  }
+
+  def expand[A](element:(Int, A)):List[A] = element match {
+    case (0, _) => Nil
+    case (n, symbol) => List(symbol) ::: expand(n-1, symbol)
+  }
+}
---
+++ src/test/scala/p12.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class p12Spec extends FlatSpec with ShouldMatchers {
+  "decode" should "expand the list to its original mode" in {
+    p12.decode(List((1,'a), (2,'b))) should equal(List('a, 'b, 'b))
+  }
+}
