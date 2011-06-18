b16c3b7e8332b76be6b00d80b1399f046275cc30
Amir Moulavi <amir.moulavi@gmail.com>
2011-06-18 13:13:46 +0200

    Problem 8 solved

---
+++ src/main/scala/p08.scala
@@ -0,0 +1,12 @@
+/**
+* @author Amir Moulavi
+* Problem 8: Eliminate consecutive duplicates of list elements.
+*/
+
+object p08 {
+	def compress[A](list:List[A]):List[A] = 
+		list.foldRight(List[A]())({ (a,b) =>
+			if ( b.isEmpty || a != b.head) a :: b
+			else b
+		})
+}
---
+++ src/test/scala/p08.scala
@@ -0,0 +1,8 @@
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+
+class p08Spec extends FlatSpec with ShouldMatchers {
+	"consecutive compress" should "return no duplicate elements" in {
+		p08.compress(List(1,1,1,2,3,3,3)) should equal (List(1,2,3))
+	}
+}
